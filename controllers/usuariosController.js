331b7fe3da1f08d903d9e650ee8e54946dddc50c
AngelMeji <joseangelmejiamedina@gmail.com>
2025-10-27 11:42:05 -0500

    Agregamos la ruta en el frontend

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -10,10 +10,15 @@ const formularioRegistro = (req, res) => {
     });
 };
 
+const registrar = (req, res) => {
+    console.log("Registrando...");
+};
+
+
 const formularioOlvidePassword = (req, res) => {
     res.render("auth/olvide-password", {
         tituloPagina: "Olvide Contraseña",
     });
 };
 
-export { formularioLogin, formularioRegistro, formularioOlvidePassword };
+export { formularioLogin, formularioRegistro, registrar, formularioOlvidePassword };
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -2,13 +2,15 @@ import express from "express";
 import { 
     formularioLogin,
     formularioRegistro,
-    formularioOlvidePassword
+    registrar,
+    formularioOlvidePassword,
 } from "../controllers/usuariosController.js";
 const router = express.Router();
 
 //Routing
 router.get("/login", formularioLogin);
 router.get("/registro", formularioRegistro);
+router.post("/registro", registrar);
 router.get("/olvide-password", formularioOlvidePassword)
 
 export default router;
