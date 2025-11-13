59c910123e2a60fedeaf58695047952029d5950a
AngelMeji <joseangelmejiamedina@gmail.com>
2025-11-13 12:03:56 -0500

    Importacion y exportacion del seeder

---
+++ seed/seeder.js
@@ -0,0 +1,54 @@
+import { exit } from "node:process";
+import categoria from "./categoria.js";
+import precio from "./precio.js";
+import Categorias from "../models/Categorias.js";
+import Precios from "../models/Precios.js";
+import db from "../config/db.js";
+
+const importarDatos = async () => {
+  try {
+    // Autenticar
+    await db.authenticate();
+
+    //Generar las columnas
+    await db.sync();
+
+    //Insertar los datos
+    await Promise.all([
+      Categorias.bulkCreate(categoria),
+      Precios.bulkCreate(precio),
+    ]);
+
+    console.log("Datos importados correctamente");
+    exit();
+  } catch (error) {
+    console.log(error);
+    exit(1);
+  }
+};
+
+const eliminarDatos = async () => {
+  try {
+    //Opcion 1
+    // await Promise.all([
+    //   Categorias.destroy({ where: {} }),
+    //   Precios.destroy({ where: {} }),
+    // ]);
+
+    //Opcion Recomendada
+    await db.sync({ force: true });
+    console.log("Eliminado correctamente!");
+    exit();
+  } catch (error) {
+    console.log(error);
+    exit(1);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importarDatos();
+}
+
+if (process.argv[2] === "-e") {
+  eliminarDatos();
+}
